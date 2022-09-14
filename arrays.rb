be1aa00944fa18ab294059be398727cd2ef9d8d8
Elle Majors <hellolovely@ellemajors>
2022-09-14 11:37:09 -0600

    Adds examples with arrays

--- arrays.rb
+++ arrays.rb
@@ -4,4 +4,11 @@ age = [ 1, 2, 3, 4]
 
 bank_statement = [ 1, 100, 150, 200]
 
-enrolllment_staus = ["yes", "no", "maybe", "not sure"]
+enrolllment_staus = ["git yes", "no", "maybe", "not sure"]
+
+you can change index positions in several ways
+.pop rmoves the last added string in the array
+.push() allows you to add
+.shift shifts the index back one
+.unshift() will add a new item to the begining of the array
+.length and .size tell you how many strings in the array
